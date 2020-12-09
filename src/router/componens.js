2f53af57899fb2b5bd3d27867ff5cc680b3ee1f4
hzn <1481202041@qq.com>
2020-12-09 09:45:59 +0800

    修改之后

--- src/router/componens.js
+++ src/router/componens.js
@@ -3,20 +3,14 @@ const My = Loadable(() => import('@/pages/my'))
 const Order = Loadable(() => import('@/pages/order'))
 const Search = Loadable(() => import('@/pages/search'))
 const Takeout = Loadable(() => import('@/pages/takeOut'))
-<<<<<<< HEAD
 const HomePage = Loadable(() => import('@/pages/homePage'))
-=======
 const Login = Loadable(() => import('@/pages/login'))
->>>>>>> ce1848b84f7e05e4a748f144a7ffde18cb32e56f
 
 export {
   My,
   Order,
   Search,
   Takeout,
-<<<<<<< HEAD
-  HomePage
-=======
+  HomePage,
   Login
->>>>>>> ce1848b84f7e05e4a748f144a7ffde18cb32e56f
 }
--- src/services/api.js
+++ src/services/api.js
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
-export default{
-    homePages:'https://elm.cangdu.org/v1/cities'
-=======
-const GetentryUrl = "https://elm.cangdu.org/v2/index_entry";//获取食品分类列表
-const Getrestaurantsurl = "https://elm.cangdu.org/shopping/restaurants";//获取商铺列表
-
-export default{
-    GetentryUrl,
-    Getrestaurantsurl,
->>>>>>> ce1848b84f7e05e4a748f144a7ffde18cb32e56f
+export default {
+    homePages: 'https://elm.cangdu.org/v1/cities',
+    GetentryUrl: "https://elm.cangdu.org/v2/index_entry",//获取食品分类列表
+    Getrestaurantsurl: "https://elm.cangdu.org/shopping/restaurants",//获取商铺列表
 }
